414d079d90a710515c75e5e498b07d213635a4a0
TYGX <44126974+tydaytygx@users.noreply.github.com>
2025-12-12 09:52:39 +0800

    Launch with virtual desktop to support multiple windows
    Updated the script to set a virtual desktop resolution and launch explorer.exe.

--- startapp.sh
+++ startapp.sh
@@ -4,9 +4,12 @@ service dbus status
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
-
 export LANG=zh_CN.UTF-8
 
 export WINEPREFIX=/config/.wine
-# exec /config/lutris-GE-Proton8-26-x86_64/bin/wine  /config/.wine/drive_c/windows/system32
-exec /usr/bin/wine  /config/.wine/drive_c/windows/system32
+
+VIRT_DESK_RESOLUTION="1920x1080"
+
+exec /usr/bin/wine explorer /desktop=shell,"$VIRT_DESK_RESOLUTION" explorer.exe
+#exec /config/lutris-GE-Proton8-26-x86_64/bin/wine  /config/.wine/drive_c/windows/system32
+#exec /usr/bin/wine  /config/.wine/drive_c/windows/system32
